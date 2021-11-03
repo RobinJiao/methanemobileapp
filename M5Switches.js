984d996a8a17e0cfce1e62eacb32b2ee3125f376
Robin Jiao <robin.jiao@gmail.com>
2021-11-03 16:16:43 +0800

    RN +firebase data update works!

--- M5Switches.js
+++ M5Switches.js
@@ -4,41 +4,22 @@ import { getDatabase, ref, onValue} from "firebase/database";
 
 const M5Switches = () => {
     const db = getDatabase();
-    const [btns, setBtns]=useState([false,false,false]);
-    let btnAReading=false,btnBReading=false,btnCReading=false;
-    console.log('useState ....M5Switches btns: ', btns);
     const dbBtnA = ref(db, 'BtnA');
     const dbBtnB = ref(db, 'BtnB');
     const dbBtnC = ref(db, 'BtnC');
 
-    onValue(dbBtnA, (snapshot)=>{
-        btnAReading=snapshot.val();
-        console.log('btnAReading: ', btnAReading);
-    })
-   
-    onValue(dbBtnB, (snapshot)=>{
-        btnBReading=snapshot.val();
-        console.log('btnBReading: ', btnBReading);
-    });
-
-    onValue(dbBtnC, (snapshot)=>{
-        btnCReading=snapshot.val();
-        console.log('btnCReading: ', btnCReading);
-    })
-    useEffect(() => {
-        setBtns([btnAReading, btnBReading, btnCReading]);
-        console.log('useEffecting ......M5Switches btns: ', [btnAReading, btnBReading, btnCReading])
-        // return () => {}
-    }, [btnAReading,btnBReading,btnCReading]);
-
-    const ShowButton=({btn,btnReading})=>{
-        const [btnValue, setBtnValue]= useState(btnReading);
+    const ShowButton=({btn,dbBtn})=>{
+        const [btnValue, setBtnValue]= useState(false);
         useEffect(() => {
-            setBtnValue(btnReading);
+            onValue(dbBtn, (snapshot)=>{
+                let btnReading=snapshot.val();
+                console.log('btn Reading: ',btn, btnReading);
+                setBtnValue(btnReading);
+            })
             return () => {
                 // cleanup
             }
-        }, [btnReading]);
+        }, [btn, dbBtn]);
         console.log('rendering ShowButton ...',btn)
         return (
             <View style={styles.buttonline}>
@@ -49,9 +30,9 @@ const M5Switches = () => {
     }
     return (
         <SafeAreaView style={styles.container}>
-            <ShowButton btn='A' btnReading={btns[0]} />
-            <ShowButton btn='B' btnReading={btns[1]} />
-            <ShowButton btn='C' btnReading={btns[2]} />
+            <ShowButton btn='AAA--' dbBtn={dbBtnA} />
+            <ShowButton btn='BBB--' dbBtn={dbBtnB}/>
+            <ShowButton btn='CCC--' dbBtn={dbBtnC} />
         </SafeAreaView>
     )
 }
